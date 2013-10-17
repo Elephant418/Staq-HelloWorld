93ba5f3429c92d718f4fc74abbbb7cfb5463ca23
Thomas ZILLIOX <thomas@zilliox.me>
2013-10-17 16:01:25 +0200

    Add the README & the LICENSE

--- HelloWorld/public/index.php
+++ HelloWorld/public/index.php
@@ -5,6 +5,6 @@ require_once( '../../vendor/autoload.php' );
 ( new \Staq\Server )
 	->launch( )
 	->addController( '/(:path)', function($path=NULL) {
-		return 'hello '.$path;
+		return 'Hello '.$path;
 	})
 	->run( );
